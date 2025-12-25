bd1b8ed37b6c576dfc0a78947e53373df6cad79c
Harsh Garg <148679895+harshgargcoder@users.noreply.github.com>
2025-12-25 10:36:29 +0530

    static-website creation
    added navbar

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Create Next App",
+  description: "A simple Next.js application with TypeScript and Google Fonts",
+};
+
+export default function RootLayout({ children, }: Readonly <{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        </body>
+    </html>
+  );
+}
